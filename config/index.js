947d8f9d1c8dab8458ff60f641deb7432b093e4b
star-seat <112999404+star-seat@users.noreply.github.com>
2022-09-19 20:21:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [oUHDa50F8Tk8ompplwupwaT6NlOg],
+  USERS: [wxid_8v4opexy80xl22],
     {
       // 想要发送的人的名字
       name: "潘潘",
